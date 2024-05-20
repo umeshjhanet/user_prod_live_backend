24a16555473a24b2ada40dfdff094ae823f1bb4c
Umesh <umeshjha.net@gmail.com>
2024-05-20 15:31:45 -0700

    test

--- index.js
+++ index.js
@@ -942,7 +942,6 @@ app.get("/userdetailedreportlocationwisecsv",  (req, res, next) => {
   }
 
   let whereClause = "";
-
   if (locationName) {
     whereClause = `WHERE locationname IN ('${locationName.join("','")}')`;
   }
